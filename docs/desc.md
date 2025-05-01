4a11074a270faa898292b6f0cbeccd956b3b1cde
shawnwwu <wslsdust@163.com>
2025-05-01 16:06:19 +0800

    feat: html/template初体验

--- docs/desc.md
+++ docs/desc.md
@@ -3,3 +3,9 @@ main.go程序入口
 1. 创建 server，http.server
 2. 创建路由监听，http.HandleFunc('/', func(w, r *) {})
 3. 启动服务 server.ListenAndServe
+
+go的 template 模板可以直接用，支持双大括号语法{{}}
+1. 创建模板目录，引入模板 http/template
+2. 组装模板目录，使用 os.Getwd() + '/template/index.html'
+3. 解析这个文件生成 templateContent: template.ParseFiles(templatePath)
+4. 执行templateContent.Execute(writer, data)
--- main.go
+++ main.go
@@ -2,8 +2,10 @@ package main
 
 import (
 	"encoding/json"
+	"html/template"
 	"log"
 	"net/http"
+	"os"
 )
 
 type IndexData struct {
@@ -12,13 +14,25 @@ type IndexData struct {
 }
 
 // ! 小写不对外暴露
-func handleIndex(w http.ResponseWriter, r *http.Request) {
+func handleHome(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 	var indexData IndexData
 	indexData.Title = "吴少的 go 博客"
 	indexData.Desc = "Welcome to the Welcome page."
 	jsonStr, _ := json.Marshal(indexData)
 	w.Write(jsonStr)
+}
+func handleIndexHtml(w http.ResponseWriter, r *http.Request) {
+	var indexData IndexData
+	indexData.Title = "吴少的 go 博客"
+	indexData.Desc = "Welcome to the Welcome page."
+	//! 获取项目根据经
+	path, _ := os.Getwd()
+	templatePath := path + "/template/index.html"
+
+	templateContent, _ := template.ParseFiles(templatePath)
+	templateContent.Execute(w, indexData)
+
 }
 func main() {
 	//! 程序入口，一个项目只能有一个入口
@@ -28,7 +42,8 @@ func main() {
 
 	}
 	// 为了能够访问，需要加上路由监听根路径 就是 localhost:8080
-	http.HandleFunc("/", handleIndex)
+	http.HandleFunc("/", handleHome)
+	http.HandleFunc("/index.html", handleIndexHtml)
 	err := server.ListenAndServe()
 	if err != nil {
 		log.Println(err)
